563f83619f588c3f2ee84d8c1c4a87c8c857cc9e
Ryan Barboza <ryanbarbozapro@gmail.com>
2024-12-23 21:45:31 +0100

    Ajout des compétence + ajout dossier déroulant

--- src/components/VsCode/SideBar/FoldersRow.jsx
+++ src/components/VsCode/SideBar/FoldersRow.jsx
@@ -1,31 +1,41 @@
 import { SideBarIcons } from "../../../svg/VsCode/components/SideBar";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../../../styles/VsCode/components/FoldersRow.css";
 
-const FoldersRows = ({ marginLeft, file, FileIcon, url }) => {
-	const navigate = useNavigate();
-  
+const FoldersRows = ({ marginLeft, file, FileIcon, url, onClick, isOpen }) => {
+	if (!url) {
+		// Pour les dossiers
+		return (
+			<div
+				className={`FoldersRows ${isOpen ? "open" : "closed"}`}
+				onClick={onClick}
+			>
+				<div className="chevronIcon" style={{ marginLeft: marginLeft }}>
+					<SideBarIcons.Chevron />
+				</div>
+				<div className="fileIcon">
+					<FileIcon />
+				</div>
+				{file && <p className="fileName">{file}</p>}
+			</div>
+		);
+	}
+
+	// Pour les fichiers
 	return (
-	  <NavLink 
-		className={({ isActive }) => {
-		  // Si 'url' est null, ne pas appliquer la classe 'active'
-		  if (!url) return 'FoldersRows'; 
-		  return isActive ? 'FoldersRows active' : 'FoldersRows'; // Appliquer 'active' si le lien est actif
-		}} 
-		to={url || '#'} // Utilisation de '#' pour les liens sans URL
-	  >
-		{!url && (
-		  <div className="chevronIcon" style={{ marginLeft: marginLeft }}>
-			<SideBarIcons.Chevron />
-		  </div>
-		)}
-		<div className="fileIcon" style={url ? { marginLeft: marginLeft } : {}}>
-		  <FileIcon />
-		</div>
-		{file && <p className="fileName">{file}</p>}
-	  </NavLink>
+		<NavLink
+			className={({ isActive }) =>
+				isActive ? "FoldersRows active" : "FoldersRows"
+			}
+			to={url}
+            
+		>
+			<div className="fileIcon" style={{ marginLeft: marginLeft }} >
+				<FileIcon />
+			</div>
+			{file && <p className="fileName">{file}</p>}
+		</NavLink>
 	);
-  };
-  
-  export default FoldersRows;
-  
+};
+
+export default FoldersRows;
--- src/components/VsCode/sections/SkillsSection.jsx
+++ src/components/VsCode/sections/SkillsSection.jsx
@@ -1,7 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "../../../styles/VsCode/components/sections/SkillsSection.css";
+
+// Import du fichier JSON
+import skillsData from "../../../data/skills.json"; // Assure-toi que le chemin est correct
+
 const SkillsSection = () => {
+	const tools = skillsData.tools;
+	const languages = skillsData.languages;
+	const frameworkLibraries = skillsData.frameworkslibraries;
+
 	return (
 		<div className="SkillsSection">
-			<h1>Skills section</h1>
+			<article className="tools">
+				<div className="header">Tools</div>
+				{tools.map((tool) => (
+					<p key={tool.id}>{tool.name}</p>
+				))}
+			</article>
+			<article className="languages">
+				<div className="header">Languages</div>
+				{languages.map((language) => (
+					<p key={language.id}>{language.name}</p>
+				))}
+			</article>
+			<article className="frameworkLibraries">
+			<div className="header">Frameworks & Librairies</div>
+				{frameworkLibraries.map((frameworkLibrary) => (
+					<p key={frameworkLibrary.id}>{frameworkLibrary.name}</p>
+				))}
+			</article>
 		</div>
 	);
 };
